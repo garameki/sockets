f389402543b8a06ba41637685f727166609f27bc
garameki <gomokumeshinomoto@gmail.com>
2018-11-15 12:31:03 +0900

    Add

---
+++ websocket-client-templog.py
@@ -0,0 +1,86 @@
+#!/usr/bin/env python3
+# _*_ coding:utf-8 _*_
+'''
+MIT License
+
+Copyright (c) 2018 garameki
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+
+------------------------------------------------------------------------------
+Copyright 2018 Hiroki Ohtani.
+
+Redistribution and use in source and binary forms, with or without modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice, this list of conditions and the following disclaimer in the documentation and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its contributors may be used to endorse or promote products derived from this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+'''
+import codecs
+import re
+import websocket
+import sys
+try:
+	import thread
+except ImportError:
+	import _thread as thread
+import time
+
+def on_message(ws, message):
+	objCommand = re.search("^20\d{6}$",message)
+	if objCommand is not None:
+		command = objCommand.group().strip()
+		filename = command+".csv"
+		fd = codecs.open("/home/pi/data/20181115.csv","r")
+		data = fd.read()
+		fd.close()
+		ws.send("CSV:"+command+":"+data)
+def on_error(ws, error):
+	print(error)
+
+def on_close(ws):
+	print("### closed ###")
+
+def on_open(ws):
+	def run(*args):
+		time.sleep(1)
+	#	ws.close()
+	#	print("thread terminating...")
+	thread.start_new_thread(run, ())
+
+
+if __name__ == "__main__":
+	args = sys.argv
+	if len(args) == 2:
+		port = args[1].strip()
+	else:
+		port = "9801"
+	print("port={}".format(port))
+	websocket.enableTrace(True)
+	ws = websocket.WebSocketApp("ws://garameki.com:"+port+"/",
+	on_message = on_message,
+	on_error = on_error,
+	on_close = on_close)
+	ws.on_open = on_open
+	ws.run_forever()
