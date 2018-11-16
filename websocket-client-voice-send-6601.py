f5348075d2fbe8c86c6e5a174691cbe4ea74edec
garameki <gomokumeshinomoto@gmail.com>
2018-11-16 19:46:04 +0900

    RENAME

--- websocket-client-voice-send-6601.py
+++ websocket-client-voice-send-6601.py
@@ -57,7 +57,9 @@ def on_message(ws, message):
 		fd = codecs.open("/home/pi/data/yoroshiku.mp3","rb")
 		data = fd.read()
 		fd.close()
-		ws.send(base64.b64encode(data))
+		ws.send(base64.b64encode(data))#correct
+		###ws.send(data)#incorrect UnicodeDecodeError: 'utf-8' codec can't decode byte 0xff in position 0: invalid start byte
+
 
 def on_error(ws, error):
 	print(error)
